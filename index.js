6b85f86fe0d19e977664654cd39380839ea33634
Sakif Hridoy <sakifworkspace@gmail.com>
2021-12-17 19:16:55 +0600

    book-now-button-and-show-data-in-console

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express')
-const app = express()
-const port = 5000
-
-
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const { MongoClient } = require('mongodb');
 const uri = "mongodb+srv://arabian:arabianHorse55@cluster0.djg6r.mongodb.net/burjAlArab?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const port = 5000;
+const app = express()
+
+app.use(cors());
+app.use(bodyParser.json());
+
 client.connect(err => {
   const collection = client.db("burjAlArab").collection("bookings");
   // perform actions on the collection object
@@ -20,4 +25,10 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
+})
+
+app.post('/addBooking',(req,res)=>{
+  const newBooking = req.body;
+  console.log(newBooking);
+
 })
