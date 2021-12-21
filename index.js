0be59fa5d37fc93d17954c7d639f157e1b5ca2ea
Sakif Hridoy <sakifworkspace@gmail.com>
2021-12-21 18:39:02 +0600

    Set environment variable and hide sensitive information

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const express = require('express')
 const { initializeApp } = require('firebase-admin/app');
+require('dotenv').config();
 // import { getAuth} from "firebase/auth";
 
 // const auth = getAuth(firebaseApp);
 // onAuthStateChanged(auth, user => {
-//   // Check for user status
+//   // Check for user status arabianHorse55
 // });
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://arabian:arabianHorse55@cluster0.djg6r.mongodb.net/burjAlArab?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.djg6r.mongodb.net/burjAlArab?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
+console.log(process.env.DB_USER)
 const port = 5000;
-const app = express()
+const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -21,11 +22,11 @@ app.use(bodyParser.json());
 
 var admin = require("firebase-admin");
 // generate key from service account of firebase and and this file to project directory and rewrite the path name pf serviceAccount var
-var serviceAccount = require("./burj-al-arab-8f7bc-firebase-adminsdk-sh6ky-e57a07ce1d.json");
+var serviceAccount = require("./configs/burj-al-arab-8f7bc-firebase-adminsdk-sh6ky-e57a07ce1d.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://burj-al-arab.firebaseio.com'
+  databaseURL: process.env.FIRE_DB
 });
 
 
